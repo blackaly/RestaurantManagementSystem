6e1d366178a39a9a7c23a0cd1f898c4697b2d81b
blackaly <napob81191@asuflex.com>
2025-02-06 20:12:20 +0200

    update

--- Areas/Dashboard/Controllers/ReservationsController.cs
+++ Areas/Dashboard/Controllers/ReservationsController.cs
@@ -3,7 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using OrderingSystem.Models.DbContext;
 using OrderingSystem.Models.DbModels;
-using OrderingSystem.Models.ViewModel;
+using OrderingSystem.Models.Enums;
+using OrderingSystem.Areas.Dashboard.Models.ViewModels.Shared;
 
 namespace OrderingSystem.Areas.Dashboard.Controllers
 {
@@ -24,9 +25,24 @@ namespace OrderingSystem.Areas.Dashboard.Controllers
             return View();
         }
 
-        public IActionResult PenddingReservations(int id)
+        public IActionResult PenddingReservations()
         {
-            return View();
+
+            var pendding = dbContext.Reservations.GroupBy(x => new { x.ReservationDate, x.PhoneNumber, x.Email, x.GuestName, x.ReservationStatus, x.NumberOfPeople } )
+                .Select(y => new ReservationViewModel
+                {
+                    id = y.FirstOrDefault().Id,
+                    ReservationDate = $"{y.Key.ReservationDate.Day}/{y.Key.ReservationDate.Month}/{y.Key.ReservationDate.Year}",
+                    ReservationTime = $"{y.Key.ReservationDate.Hour}:{y.Key.ReservationDate.Minute}",
+                    NumberOfPeople = y.Key.NumberOfPeople,
+                    GuestName = y.Key.GuestName,
+                    PhoneNumber = y.Key.PhoneNumber,
+                    Email = y.Key.Email,
+                    NumberOfTablesInSameDateTime = y.Count(),
+                    ReservationStatus = y.Key.ReservationStatus
+                }).Where(z => z.ReservationStatus == ReservationStatus.Pendding).ToList();
+
+            return View(pendding);
         }
 
         
---
+++ Areas/Dashboard/Models/ViewModels/Shared/ReservationViewModel.cs
@@ -0,0 +1,38 @@
+﻿using OrderingSystem.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderingSystem.Areas.Dashboard.Models.ViewModels.Shared
+{
+    public class ReservationViewModel
+    {
+            
+            public int id { get; set;}
+
+            [Required]
+            [Display(Name = "Reservation Date")]
+            public string ReservationDate { get; set; }
+            [Required]
+            [Display(Name = "Time")]
+            public string ReservationTime { get; set; }
+            [Required]
+            [Display(Name = "Number of People")]
+            public int NumberOfPeople { get; set; }
+            [Required]
+            [Display(Name = "Guest Name")]
+            public string GuestName { get; set; }
+            [Required]
+            [Display(Name = "Phone Number")]
+            [DataType(DataType.PhoneNumber)]
+            public string PhoneNumber { get; set; }
+            [Required]
+            [DataType(DataType.EmailAddress)]
+            public string Email { get; set; }
+            [Display(Name = "Special Requests (Optional)")]
+            public string? Comment { get; set; }
+            public int NumberOfTablesInSameDateTime { get; set; }
+            public ReservationStatus ReservationStatus { get; set; }
+
+
+        }
+
+}
